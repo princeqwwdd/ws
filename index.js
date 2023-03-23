cf04b570b8f4c514ae17135849cda97d05ab3396
princeqwwdd <128700604+princeqwwdd@users.noreply.github.com>
2023-03-23 20:52:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,61 +1,18 @@
-const path = require("path");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const { init: initDB, Counter } = require("./db");
+var express = require("express")
+var app =express()
+var expressWs = require("express-ws")
+var wss = expressWs(app)
+var aWss = wss.getWss("/")
+app = wss.app
+app.ws("/",(ws,req)=>{
+    ws.onmessage = (msg)=>{
+        console.log(msg.data.toString());
+        aWss.clients.forEach((client)=>{
+            client.send("来自服务端消息")
+        })
+    }
+})
 
-const logger = morgan("tiny");
-
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(cors());
-app.use(logger);
-
-// 首页
-app.get("/", async (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// 更新计数
-app.post("/api/count", async (req, res) => {
-  const { action } = req.body;
-  if (action === "inc") {
-    await Counter.create();
-  } else if (action === "clear") {
-    await Counter.destroy({
-      truncate: true,
-    });
-  }
-  res.send({
-    code: 0,
-    data: await Counter.count(),
-  });
-});
-
-// 获取计数
-app.get("/api/count", async (req, res) => {
-  const result = await Counter.count();
-  res.send({
-    code: 0,
-    data: result,
-  });
-});
-
-// 小程序调用，获取微信 Open ID
-app.get("/api/wx_openid", async (req, res) => {
-  if (req.headers["x-wx-source"]) {
-    res.send(req.headers["x-wx-openid"]);
-  }
-});
-
-const port = process.env.PORT || 80;
-
-async function bootstrap() {
-  await initDB();
-  app.listen(port, () => {
-    console.log("启动成功888888", port);
-  });
-}
-
-bootstrap();
+app.listen(80,(err)=>{
+    console.log("服务启动成功");
+})
